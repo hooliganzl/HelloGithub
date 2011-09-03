d4a3cd906fc670f27f49e4181099bb5ac6a7b2ef
Hooliganzl <thaihooligan@hotmail.com>
2011-09-03 10:24:38 +0700

    First Commit
    Window Based Template

---
+++ HelloGithub/HelloGithubAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HelloGithubAppDelegate.h
+//  HelloGithub
+//
+//  Created by Anak Mirasing on 9/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloGithubAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
